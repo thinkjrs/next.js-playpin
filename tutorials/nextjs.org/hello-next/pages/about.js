2c3df0d484cb7c6cfb3c7040c889ebc16f1c5892
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-03-26 14:00:13 -0500

    add variable declaration of page content

--- tutorials/nextjs.org/hello-next/pages/about.js
+++ tutorials/nextjs.org/hello-next/pages/about.js
@@ -1,10 +1,15 @@
-import Layout from '../components/DefaultLayout';
+import Layout from '../components/ContentPropLayout';
+
+
+const aboutPageContent =
+  <div>
+  <h1><strong>This</strong> is the about page</h1>
+  <p><small><code>Using Next.js.</code></small></p>
+  </div>;
 
 export default function About() {
     return (
-      <Layout>
-        <h1><strong>This</strong> is the about page</h1>
-        <p><small><code>Using Next.js.</code></small></p>
+      <Layout content={aboutPageContent}>
       </Layout>
     );
 }
