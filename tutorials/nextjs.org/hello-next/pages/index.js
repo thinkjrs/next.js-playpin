d339d7af38cceb6b6d309b73675521e7e08b6f64
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-03-26 14:10:40 -0500

    add content property exemplar

--- tutorials/nextjs.org/hello-next/pages/index.js
+++ tutorials/nextjs.org/hello-next/pages/index.js
@@ -3,14 +3,27 @@
  * Sample index page from the nextjs.org/learn tutorials.
  *
  */
+// basic exemplar
+//import Layout from '../components/DefaultLayout';
+//
+//export default function Index() {
+//    return (
+//        <Layout>
+//          <h1>Hello, world!</h1>
+//          <p><small><code>Using Next.js.</code></small></p>
+//        </Layout>
+//    );
+//}
 
-import Layout from '../components/DefaultLayout';
+// content as prop exemplar
+import Layout from '../components/ContentPropLayout';
+
+const indexPageContent = 
+  <div>
+    <h1>Hello, world!</h1>
+    <p><small><code>Using Next.js.</code></small></p>
+  </div>;
 
 export default function Index() {
-    return (
-        <Layout>
-          <h1>Hello, world!</h1>
-          <p><small><code>Using Next.js.</code></small></p>
-        </Layout>
-    );
+  return <Layout content={indexPageContent} />;
 }
