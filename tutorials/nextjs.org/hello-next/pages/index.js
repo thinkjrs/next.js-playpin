b187ef4b1a6e6593fb4000fc4ca18e07fbeeb893
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-03-26 15:59:40 -0500

    add initial code for batman data example

--- tutorials/nextjs.org/hello-next/pages/index.js
+++ tutorials/nextjs.org/hello-next/pages/index.js
@@ -16,48 +16,87 @@
 //}
 
 // content as prop exemplar
-import Layout from '../components/ContentPropLayout';
+import Layout from '../components/DefaultLayout';
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch';
 
-
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
-
-const blogPageContent = 
-<div>
-  <h1>
-    Hello, blog world!
-  </h1>
-  <ul>
-    <PostLink title="Hello Next.js!" />
-    <PostLink title="Why nextjs.org/learn is awesome." />
-    <PostLink title="Musicfox is the best." />
-  </ul>
-</div>;
-
-const indexPageContent = 
-<div>
-  <h1>
-    Hello, world!
-  </h1>
-  <p>
-    <small>
-      <code>
-        Using Next.js.
-      </code>
-    </small>
-  </p>
-</div>;
+//const PostLink = props => (
+//  <li key={show.id}>
+//    <Link href="/p/[id]" as={`/p/${show.id}`}>
+//      <a>{show.name}</a>
+//    </Link>
+//  </li>
+//);
+//
+//const blogPageContent = 
+//<div>
+//  <h1>
+//    Batman TV Breakdown
+//  </h1>
+//  <ul>
+//    {props.shows.map(show => (
+//      <PostLink />
+//    ))}
+//  </ul>
+//</div>;
+//
+//const indexPageContent = 
+//<div>
+//  <h1>
+//    Hello, world!
+//  </h1>
+//  <p>
+//    <small>
+//      <code>
+//        Using Next.js.
+//      </code>
+//    </small>
+//  </p>
+//</div>;
 
 //export default function Index() {
 //  return <Layout content={indexPageContent} />;
 //}
 
-export default function Blog() {
-  return <Layout content={blogPageContent} />;
-}
+// export default function Blog() {
+//   return <Layout content={blogPageContent} />;
+// }
+// 
+// PostLink.getInitialProps = async function() {
+//   const res = await fetch('https://api.tvmae.com/search/show?q=batman');
+//   const data = await res.json();
+// 
+//   console.log(`Data fetched. Count: ${data.length}`)
+// 
+//   return {
+//     shows: data.map(entry => entry.show)
+//   };
+// };
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
