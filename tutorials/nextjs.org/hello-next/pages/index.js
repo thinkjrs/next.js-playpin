554a7b4959acb2bdd476cbccd37b6f57aebad18b
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-03-26 14:25:27 -0500

    add blogPageContent and PostLink

--- tutorials/nextjs.org/hello-next/pages/index.js
+++ tutorials/nextjs.org/hello-next/pages/index.js
@@ -17,13 +17,47 @@
 
 // content as prop exemplar
 import Layout from '../components/ContentPropLayout';
+import Link from 'next/link'
+
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+const blogPageContent = 
+<div>
+  <h1>
+    Hello, blog world!
+  </h1>
+  <ul>
+    <PostLink title="Hello Next.js!" />
+    <PostLink title="Why nextjs.org/learn is awesome." />
+    <PostLink title="Musicfox is the best." />
+  </ul>
+</div>;
 
 const indexPageContent = 
-  <div>
-    <h1>Hello, world!</h1>
-    <p><small><code>Using Next.js.</code></small></p>
-  </div>;
+<div>
+  <h1>
+    Hello, world!
+  </h1>
+  <p>
+    <small>
+      <code>
+        Using Next.js.
+      </code>
+    </small>
+  </p>
+</div>;
 
-export default function Index() {
-  return <Layout content={indexPageContent} />;
+//export default function Index() {
+//  return <Layout content={indexPageContent} />;
+//}
+
+export default function Blog() {
+  return <Layout content={blogPageContent} />;
 }
